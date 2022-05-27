b7574b02ffcc0cd76673e598515e08868fa57fe5
leyalutik <ludapevek@gmail.com>
2022-05-27 11:00:52 +0300

    date hehehe

--- START_DAY.bat
+++ START_DAY.bat
@@ -2,128 +2,19 @@ chcp 65001
 
 @Echo off
 
-echo Начало работы!
-echo Убедитесь, что имя вашего файла START_DAY.bat.
-
-::Если переменной X нет то установить X=Китай
-if not defined X ( set X=Китай)  else  ( echo variable X is already defined by %X% )
-if not defined XX (set XX="%USERPROFILE%\Desktop\Китай"  ) else ( echo variable XX is already defined by %XX% )
+::echo Начало работы!
+echo Start working!
+::echo Убедитесь, что имя вашего файла START_DAY.bat.
+echo Be sure the name of executable file is START_DAY.bat
 
 
-rem Связанное с браузером Firefox
-
-::if not defined pf (set pf="%ProgramFiles%\Mozilla Firefox\firefox.exe" ) else ( echo variable pf is already defined by %pf% )
-::pause
-::if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" 
-
-set pf="%ProgramFiles%\Mozilla Firefox\firefox.exe" else 
-
-set pf="%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" 
-
- 
-echo Создание папки Китай в %USERPROFILE%\Desktop
+echo Deleting directory git from directory Downloads
+::Удаление папки git из папки Загрузки
 pause
-cd  %USERPROFILE%\Desktop
-mkdir %X%
-
-
-echo Переход на сайт www.github.com, где нужно  скачать репозиторий git.zip в папку Загрузки
-pause
-if exist  "%ProgramFiles%\Mozilla Firefox\firefox.exe"  set resF=true
-if exist  "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" set resF=true
-if %resF% == true start "" firefox.exe  "https://github.com/leyalutik/git.git" 
-else start "" "https://github.com/leyalutik/git.git" 
-::Open with browser chrome
-::else if exist  "%ProgramFiles%\Google\Chrome\Application\chrome.exe"  set resCh=true
-::else if exist  "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"  set resCh=true
-::if %resCh% == true start "" chrome.exe  "https://github.com/leyalutik/git.git" else start ""  "https://github.com/leyalutik/git.git"
-
-echo Переименование скачанного файла git-master.zip в git.zip
-pause
-cd %USERPROFILE%\Downloads
-RENAME git-master.zip git.zip
-
-
-echo Распакование файлов git.zip в папке Загрузки
-pause
-cd %USERPROFILE%\Downloads
-tar -xf  git.zip
-
-echo Переименование папки git-master в git
-pause
-cd %USERPROFILE%\Downloads
-RENAME git-master git
-
-echo Копирование папки git в C:\git
-pause
-cd %USERPROFILE%\Downloads
-XCOPY  git C:\git\ /E
-
-
-echo Добавление переменных среды в переменную Path
-echo Сохраним переменную в файле "current_system_path.txt"
-cd C:\git
-set Path > current_system_path.txt
-pause
-
-echo Открываем "Панель управления.Система".
-pause
-
-control /name Microsoft.System
-echo Перейдите в окно "О системе" -^> "Дополнительные параметры" -^> "Переменные среды"
-echo В верхнем окне переменные пользователя выберите Path и нажмите кнопку "Изменить".Потом нажмите 'Enter'.
-pause
-
-echo Добавление переменных в переменную Путь.
-pause
-
-
-echo Скопирован в буфер Путь:"C:\git\bin"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\bin| clip
-pause
-
-echo Скопирован в буфер Путь:"C:\git\mingw\bin"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\mingw\bin| clip
-pause
-
-echo Скопирован в буфер Путь:"C:\git\leya\cmake\bin"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\mingw\cmake\bin| clip
-pause
-
-echo Скопирован в буфер Путь:"C:\git\mingw\vim82"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\mingw\vim82| clip
-pause
-
-
-echo Скопирован в буфер Путь:"C:\git\leya\notion_set"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\leya\notion_set| clip
-pause
-
-echo Скопирован в буфер Путь:"C:\git\leya"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\leya| clip
-pause
-
-echo Скопирован в буфер Путь:"C:\git\leya\WORK"
-echo Нажмите кнопку "Создать" и добавьте эту переменную через ctrl+v. Потом нажмите Enter.
-echo C:\git\leya\WORK| clip
-pause
-
-echo Нажимайте ОК на всех окнах.
-pause
-
-
-
-echo Удаление папки git из папки Загрузки
-pause
-cd %USERPROFILE%\Downloads
+cd "%USERPROFILE%\Downloads"
 RMDIR /S git
-echo Удаление архива git.zip из папки Загрузки  
+echo Deleting archive git.zip from directory Downloads 
+::Удаление архива git.zip из папки Загрузки  
 pause
 del git.zip
 
@@ -131,7 +22,8 @@ del git.zip
 
 
 
-echo Создание исполняемого файла ADD_REPOSITORIES
+echo Creating bat file ADD_REPOSITORIES 
+::Создание исполняемого файла ADD_REPOSITORIES
 pause
 cd C:\git
 echo @Echo off > ADD_REPOSITORIES.bat
@@ -198,15 +90,15 @@ pause
 
 echo Копирование стартового исполняемого файла в C:\git\leya и папку Китай
 pause
-cd %USERPROFILE%\Desktop\
+cd "%USERPROFILE%\Desktop"
 copy START_DAY.bat C:\git\leya\START_DAY.bat
-copy START_DAY.bat %X%\START_DAY.bat
+copy START_DAY.bat "%X%\START_DAY.bat"
 
 echo Задания на вечер, создание файла.
 cd C:\git\leya
 copy evening_tasks.txt "%USERPROFILE%\Desktop\Китай\evening_tasks.txt"
 pause
-cd %XX%
+cd "%XX%"
 start  evening_tasks.txt
 pause
 
