a5735e2cb4951616ba324cd317a9474b9afa02fc
leyalutik <ludapevek@gmail.com>
2023-01-30 20:12:04 +0300

    %date% %time%

---
+++ Dynamicheskoechtenie/bricks.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <map>
+#include <set>
+#include<exception>
+#include <cassert>
+#include<cstdlib>
+#include<iostream>
+#include<string>
+#include <sstream>
+#include <fstream>
+#include <chrono>
+
+#include "bricks.h"
+
+void time_stop(const int32_t milliseconds)
+{
+	auto start = std::chrono::steady_clock::now();
+	auto finish = std::chrono::steady_clock::now();
+	while(std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count()
+        <= miliseconds)
+{
+	finish = std::chrono::steady_clock::now();
+}
+  
+}
